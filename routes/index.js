0c1648199c877c6380c668b3e19b0ebb2b79a6e4
Velociraptor <velociraptor@students.olin.edu>
2013-03-27 21:42:41 -0400

    updating text parse function

--- routes/index.js
+++ routes/index.js
@@ -138,10 +138,32 @@ function saveToDB (obj) {
 
 function textParse (inputTweet, inputMovie) {
 	var common_string = 'a,able,about,across,after,all,almost,also,am,among,an,and,any,are,as,at,be,because,been,but,by,can,cannot,could,dear,did,do,does,either,else,ever,every,for,from,get,got,had,has,have,he,her,hers,him,his,how,however,i,if,in,into,is,it,its,just,least,let,like,likely,may,me,might,most,must,my,neither,no,nor,not,of,off,often,on,only,or,other,our,own,rather,said,say,says,she,should,since,so,some,than,that,the,their,them,then,there,these,they,this,tis,to,too,twas,us,wants,was,we,were,what,when,where,which,while,who,whom,why,will,with,would,yet,you,your';
-	var common_list = common_string.split(',');
-	var new_keywords = [];
+	var common_punc = '.,?!&()';
+
 	//get words not in common list from inputTweet
-	var movie = Movie.find({'name': inputMovie}).exec(function(err, movie){
-		//add new keywords to tags of movie, or increment hit counter if not new words
+	var new_list = inputTweet.split(' ');
+	var new_keywords = [];
+	new_list.forEach(function(word) {
+		if (common_punc.indexOf(word[word.length - 1]) != -1) {
+			word = word.slice(0,word.length-1);
+		}
+		if (common_string.indexOf(word) == -1) {
+			new_keywords.push(word);
+		}
+	});
+	console.log(new_keywords);
+	
+	//add new keywords to tags of movie, or increment hit counter if not new word
+	var movie = Movie.find({'name': inputMovie}).exec(function(err, movie) {
+		new_keywords.forEach(function(keyword) {
+			if (!(keyword in movie.keywords)) {
+				movie.keywords[keyword] += 1;
+			} else {
+				movie.keywords.push({keyword:1});
+			};
+		});
+		console.log(movie.keywords);
+	})
+	//Movie.update({'name': inputMovie}, {'tags':new_tags}).exec(function(err, movie1){
 	});
 }
